31c8e3ec48ae40d72c79147a2f162d15544c8fcb
ONUR DERMAN <onrdrmn@gmail.com>
2023-04-25 02:37:54 +0300

    Binary Search Algorithm Implemented

---
+++ BinarySearch/OriginalAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿namespace BinarySearch1;
+
+internal class OriginalAlgorithm
+{
+    public static int BinarySearch(int[] arr, int target)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            else if (arr[mid] < target)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,41 @@
+﻿
+int[] arr = new int[40];
+
+for (int i = 0; i < 40; i++)
+    arr[i] = i + 1;
+
+int target = 38;
+Console.WriteLine($"Array: {string.Join(", ", arr)}");
+Console.WriteLine($"Target value: {target}");
+
+int left = 0, right = arr.Length - 1, step = 0;
+
+while (left <= right)
+{
+    int mid = left + (right - left) / 2;
+
+    Console.WriteLine($"\nStep {++step}:");
+    Console.WriteLine($"Left index: {left}");
+    Console.WriteLine($"Right index: {right}");
+    Console.WriteLine($"Middle index: {mid}");
+
+    if (arr[mid] == target)
+    {
+        Console.WriteLine($"Target value found at index: {mid}");
+        return;
+    }
+    else if (arr[mid] < target)
+    {
+        left = mid + 1;
+        Console.WriteLine("Target value is greater than middle value.");
+    }
+    else
+    {
+        right = mid - 1;
+        Console.WriteLine("Target value is less than middle value.");
+    }
+}
+
+Console.WriteLine("Target value not found in the array.");
+
+Console.ReadKey();
